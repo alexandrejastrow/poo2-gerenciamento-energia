5348b36555f891cec89dedd1feef96ac74c2acc0
alexandrejastrow <52933958+alexandrejastrow@users.noreply.github.com>
2020-12-31 17:16:15 -0300

    add users ao banco de dados CSV

---
+++ src/main/java/db/Db.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package db;
+
+import Models.User;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author alexandre jastrow da cruz
+ */
+public class Db {
+    
+    private final ReadWriteCSV file;
+    private LinkedList<User> users;
+    //private LinkedList<Local> locals;
+    //private LinkedList<Equipament> equipaments;
+    //private LinkedList<Sensor> sensors;
+    
+    public Db(){
+        file = new ReadWriteCSV();
+    }
+    
+    public void loadUsers(){
+    
+        this.users = file.getUsers();
+
+    }
+    public boolean saveUsers(){
+        
+        return file.saveUsers(users);
+    }
+    
+    public boolean insert(User user){
+        
+        if(users.add(user)){
+            saveUsers();
+            return true;
+        }else{
+            return false;
+        } 
+    }
+    public User selectUser(String login){
+        
+        for(int i =0; i< users.size(); i++){
+            
+            if(users.get(i).getLogin().equals(login)){
+                return users.get(i);
+            }
+        }
+        
+        return null;
+    }
+
+}
---
+++ src/main/java/db/ReadWriteCSV.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package db;
+
+import Models.User;
+import Models.Users.Usuario;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author alexandre jastrow da cruz
+ */
+public class ReadWriteCSV {
+    
+    private String header;
+    ReadWriteCSV() {
+        
+    }
+    
+    LinkedList<User> getUsers(){
+        
+        try {
+            User us;
+            
+            LinkedList<User> clientes = new  LinkedList<>();
+            
+            String current = new java.io.File( "." ).getCanonicalPath();
+            File inputFile = new File(current + "\\src\\main\\java\\db\\Files\\users.csv");
+            
+            try (FileReader in = new FileReader(inputFile); BufferedReader arq = new BufferedReader(in)) {
+                
+                String line = arq.readLine();
+                this.header = line;
+                
+                while ((line = arq.readLine()) != null) {
+                    
+                    String[] vars = line.split(";");
+                    
+                    us = Usuario.newUser(vars[1], vars[2], vars[3], Integer.valueOf(vars[4]));
+                    
+                    clientes.add(us);                  
+                    
+                }
+                
+            } catch (Exception ex) {
+                Logger.getLogger(ReadWriteCSV.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            return clientes;
+            
+            
+            
+        } catch (IOException ex) {
+            Logger.getLogger(ReadWriteCSV.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    
+    public boolean saveUsers(LinkedList<User> users){
+        
+        try {
+            String current = new java.io.File( "." ).getCanonicalPath();
+            File outputFile = new File(current + "\\src\\main\\java\\db\\Files\\users.csv");
+            
+            try (FileWriter arq = new FileWriter(outputFile)) {
+                PrintWriter gravaArq = new PrintWriter(arq);
+                
+                gravaArq.println(this.header);
+                
+                int id;
+                String tpUser;
+                String login;
+                String senha;
+                int permissao;
+                
+                for(int i = 0; i< users.size(); i++){
+                    
+                    id = users.get(i).getId();
+                    tpUser = users.get(i).getTipoUsuario();
+                    login = users.get(i).getLogin();
+                    senha = users.get(i).getPassword();
+                    permissao = users.get(i).getForcaDePermissao();
+                    
+                    String aux = id + ";" + tpUser + ";" + login + ";" + senha + ";" + permissao;
+                    gravaArq.println(aux);
+                }
+            }
+            
+            return true;
+        } catch (IOException ex) {
+            Logger.getLogger(ReadWriteCSV.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+}
